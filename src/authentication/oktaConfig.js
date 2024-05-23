bbd7a5a289d502957f1782399cb9667dc3277e42
Sumant Kumar <ruchikeshri912@gmail.com>
2024-05-23 16:23:36 +0530

    Okta Change

--- src/authentication/oktaConfig.js
+++ src/authentication/oktaConfig.js
@@ -1,7 +1,7 @@
 export const oktaConfig = {
-  clientId: `0oaehpsdrnil8RjBc697`,
+  clientId: `0oaejwujxqLPUuc0F697`,
   issuer: `https://trial-9524431.okta.com/oauth2/default`,
-  redirectUri: `http://localhost:3000/sign/callback`, // this makes it so redirects to login if not logged in for secure routes
+  redirectUri: `https://ruchi912.github.io/sign/callback`, // this makes it so redirects to login if not logged in for secure routes
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
